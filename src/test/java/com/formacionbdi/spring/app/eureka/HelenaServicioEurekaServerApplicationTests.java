cc6bc2dcfcde844a6297a5ebe17732b1b82e271e
kurama027 <64025846+kurama027@users.noreply.github.com>
2022-07-14 11:42:21 -0500

    servidor eureka

---
+++ src/test/java/com/formacionbdi/spring/app/eureka/HelenaServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.spring.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelenaServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
